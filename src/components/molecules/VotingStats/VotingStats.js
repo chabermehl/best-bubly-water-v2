a1c5fdecf2733a0b7305372a2da5b6895fa98d4e
chabermehl <clp.habermehl@gmail.com>
2020-06-08 22:54:32 -0600

    Add basic voting stats query

--- src/components/molecules/VotingStats/VotingStats.js
+++ src/components/molecules/VotingStats/VotingStats.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {useQuery} from '@apollo/react-hooks';
+import {gql} from 'apollo-boost';
+
+const GET_VOTE_STATS = gql`
+  query vote_average {
+    flavors_aggregate {
+      aggregate {
+        avg {
+          votes
+        }
+      }
+    }
+  }
+`;
 
 const VotingStats = () => {
-  return <div>did this work?</div>;
+  const {loading, error, data} = useQuery(GET_VOTE_STATS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const {votes} = data.flavors_aggregate.aggregate.avg;
+
+  return <div>Average: {votes}</div>;
 };
 
 export default VotingStats;
