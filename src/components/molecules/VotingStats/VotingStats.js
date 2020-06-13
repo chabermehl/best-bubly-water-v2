3f2fe29fb64c2daf798562cad1269d7442398092
chabermehl <clp.habermehl@gmail.com>
2020-06-12 18:46:34 -0600

    Add table to statistics page

--- src/components/molecules/VotingStats/VotingStats.js
+++ src/components/molecules/VotingStats/VotingStats.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useQuery} from '@apollo/react-hooks';
 import {gql} from 'apollo-boost';
+import {Table} from 'semantic-ui-react';
 
 const GET_VOTE_STATS = gql`
   query vote_average {
@@ -21,7 +22,23 @@ const VotingStats = () => {
   if (error) return <p>Error :(</p>;
   const {votes} = data.flavors_aggregate.aggregate.avg;
 
-  return <div>Average: {votes}</div>;
+  return (
+    <Table celled>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Category</Table.HeaderCell>
+          <Table.HeaderCell>Value</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+
+      <Table.Body>
+        <Table.Row>
+          <Table.Cell>Votes</Table.Cell>
+          <Table.Cell>{votes}</Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table>
+  );
 };
 
 export default VotingStats;
